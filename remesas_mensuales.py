c95a5c943ee769ba28453daaebf840603a448aba
Montse <lapanquecita@proton.me>
2026-07-01 16:02:03 -0600

    Actualización datos

--- remesas_mensuales.py
+++ remesas_mensuales.py
@@ -18,14 +18,51 @@ from statsmodels.tsa.seasonal import STL
 
 
 # Mes y año en que se recopilaron los datos.
-FECHA_FUENTE = "junio 2026"
+FECHA_FUENTE = "julio 2026"
 
 # Mes y año del IPC de referencia.
-FECHA_INFLACION = "abril de 2026"
+FECHA_INFLACION = "mayo de 2026"
 
 # Paleta de colores para todas las gráficas.
 PLOT_COLOR = "#1C1F1A"
 PAPER_COLOR = "#262B23"
+
+
+def cargar_deflactadores(modo):
+    """
+    Carga las series de tiempo del IPC y tipo de cambio
+    en un solo DataFrame.
+
+    Esto es utilizado para ajustar las remesas por inflación.
+
+    Parameters
+    ----------
+    modo : str
+        Puede ser 'mensual' o 'trimestral'.
+
+    """
+
+    # Cargamos el dataset del IPC.
+    ipc = pd.read_csv("./assets/IPC.csv", parse_dates=["PERIODO"], index_col=0)
+
+    # Escogemos un IPC de referencia (el más reciente).
+    ipc_referencia = ipc["GENERAL"].iloc[-1]
+
+    # Calculamos el factor.
+    ipc["factor"] = ipc_referencia / ipc["GENERAL"]
+
+    # Cargamos el dataset del tipo de cambio.
+    fx = pd.read_csv("./assets/USDMXN.csv", parse_dates=["PERIODO"], index_col=0)
+
+    # Unimos ambos DataFrames y quitamos las filas incompletas.
+    df = pd.concat([ipc["factor"], fx], axis=1).dropna(axis=0)
+
+    # Si la modalidad es mensual, regresamos el DataFrame tal cual.
+    # Si es trimestral, hacemos remuestreo usando el promedio trimestral.
+    if modo == "mensual":
+        return df
+    elif modo == "trimestral":
+        return df.resample("QS").mean()
 
 
 def plot_mensuales(flujo):
@@ -212,8 +249,6 @@ def plot_pesos(flujo):
         remesas enviadas o recibidas desde México.
 
     """
-    # Cargamos el dataset del tipo de cambio.
-    fx = pd.read_csv("./assets/USDMXN.csv", parse_dates=["PERIODO"], index_col=0)
 
     # Cargamos el dataset de las remesas mensuales.
     df = pd.read_csv(
@@ -226,11 +261,12 @@ def plot_pesos(flujo):
     # Convertimos las cifras a millones de dólares.
     df["VALOR_USD"] /= 1000000
 
-    # Agregamos el tipo de cambio mensual.
-    df["cambio"] = fx["TIPO_CAMBIO"]
+    # Cargamos factor de inflación y tipo de cambio mensual.
+    df = df.join(cargar_deflactadores("mensual"))
 
-    # Hacemos la conversión a pesos.
-    df["pesos"] = df["VALOR_USD"] * df["cambio"]
+    # Hacemos la conversión a pesos corrientes.
+    # Para esta función no ajustaremos por inflación.
+    df["pesos"] = df["VALOR_USD"] * df["TIPO_CAMBIO"]
 
     # Calculamos el total de remesas por año para los últimos 10 años.
     por_año = df.resample("YS").sum().tail(10)
@@ -394,18 +430,6 @@ def plot_real(flujo):
 
     """
 
-    # Cargamos el dataset del IPC.
-    ipc = pd.read_csv("./assets/IPC.csv", parse_dates=["PERIODO"], index_col=0)
-
-    # Escogemos un IPC de referencia (el más reciente)
-    ipc_referencia = ipc["GENERAL"].iloc[-1]
-
-    # Calculamos el factor.
-    ipc["factor"] = ipc_referencia / ipc["GENERAL"]
-
-    # Cargamos el dataset del tipo de cambio.
-    fx = pd.read_csv("./assets/USDMXN.csv", parse_dates=["PERIODO"], index_col=0)
-
     # Cargamos el dataset de las remesas mensuales.
     df = pd.read_csv(
         "./data/remesas_mensuales.csv", parse_dates=["PERIODO"], index_col=0
@@ -417,17 +441,11 @@ def plot_real(flujo):
     # Convertimos las cifras a millones de dólares.
     df["VALOR_USD"] /= 1000000
 
-    # Agregamos el tipo de cambio mensual.
-    df["cambio"] = fx["TIPO_CAMBIO"]
+    # Cargamos factor de inflación y tipo de cambio mensual.
+    df = df.join(cargar_deflactadores("mensual"))
 
-    # Hacemos la conversión a pesos.
-    df["pesos"] = df["VALOR_USD"] * df["cambio"]
-
-    # Agregamos la columna de inflación.
-    df["inflacion"] = ipc["factor"]
-
-    # Ajustamos por inflación para obtener pesos reales.
-    df["real"] = df["pesos"] * df["inflacion"]
+    # Ajustamos por inflación y tipo de cambio para obtener pesos reales.
+    df["real"] = df["VALOR_USD"] * df["factor"] * df["TIPO_CAMBIO"]
 
     # Calculamos el total de remesas por año para los últimos 10 años.
     por_año = df.resample("YS").sum().tail(10)
@@ -492,7 +510,7 @@ def plot_real(flujo):
     )
 
     fig.update_yaxes(
-        title=f"Millones de pesos reales (precio base, {FECHA_INFLACION})",
+        title=f"Millones de pesos constantes (base, {FECHA_INFLACION})",
         tickformat="s",
         separatethousands=True,
         ticks="outside",
@@ -591,18 +609,6 @@ def plot_real_anual(flujo):
 
     """
 
-    # Cargamos el dataset del IPC.
-    ipc = pd.read_csv("./assets/IPC.csv", parse_dates=["PERIODO"], index_col=0)
-
-    # Escogemos un IPC de referencia (el más reciente)
-    ipc_referencia = ipc["GENERAL"].iloc[-1]
-
-    # Calculamos el factor.
-    ipc["factor"] = ipc_referencia / ipc["GENERAL"]
-
-    # Cargamos el dataset del tipo de cambio.
-    fx = pd.read_csv("./assets/USDMXN.csv", parse_dates=["PERIODO"], index_col=0)
-
     # Cargamos el dataset de las remesas mensuales.
     df = pd.read_csv(
         "./data/remesas_mensuales.csv", parse_dates=["PERIODO"], index_col=0
@@ -611,17 +617,11 @@ def plot_real_anual(flujo):
     # Seleccionamos solo los registros del tipo de flujo indicado.
     df = df[df["FLUJO"] == flujo]
 
-    # Agregamos el tipo de cambio mensual.
-    df["cambio"] = fx["TIPO_CAMBIO"]
+    # Cargamos factor de inflación y tipo de cambio mensual.
+    df = df.join(cargar_deflactadores("mensual"))
 
-    # Hacemos la conversión a pesos.
-    df["pesos"] = df["VALOR_USD"] * df["cambio"]
-
-    # Agregamos la columna de inflación.
-    df["inflacion"] = ipc["factor"]
-
-    # Ajustamos por inflación para obtener pesos reales.
-    df["real"] = df["pesos"] * df["inflacion"]
+    # Ajustamos por inflación y tipo de cambio para obtener pesos reales.
+    df["real"] = df["VALOR_USD"] * df["factor"] * df["TIPO_CAMBIO"]
 
     # Calculamos el total de remesas por año.
     df = df.resample("YS").sum()
@@ -725,6 +725,7 @@ def plot_real_anual(flujo):
                 bordercolor="#EEEEEE",
                 borderwidth=1.5,
                 borderpad=7,
+                font_size=22,
                 bgcolor=PLOT_COLOR,
                 align="left",
                 text=f"<b>Metodología:</b><br>Se convirtieron los dólares a pesos utilizando<br>el tipo de cambio de cada mes, se ajustó por<br>inflación a valores de {FECHA_INFLACION} y se<br>agruparon los resultados de forma anual.",
@@ -762,6 +763,185 @@ def plot_real_anual(flujo):
     fig.write_image(f"./remesas_anuales_reales_{flujo.lower()}.png")
 
 
+def remesa_promedio(flujo):
+    """
+    Genera una gráfica de oruga mostrando la
+    evolución del motno promedio por remesa.
+
+    Parameters
+    ----------
+    flujo : str
+        Puede ser "Ingresos"|"Egresos" para diferenciar entre
+        remesas enviadas o recibidas desde México.
+
+    """
+
+    # Cargamos el dataset de las remesas mensuales.
+    df = pd.read_csv(
+        "./data/remesas_mensuales.csv", parse_dates=["PERIODO"], index_col=0
+    )
+
+    # Seleccionamos solo los registros del tipo de flujo indicado.
+    df = df[df["FLUJO"] == flujo]
+
+    # Cargamos factor de inflación y tipo de cambio mensual.
+    df = df.join(cargar_deflactadores("mensual"))
+
+    # Ajustamos por inflación y tipo de cambio para obtener pesos reales.
+    df["real"] = df["VALOR_USD"] * df["factor"] * df["TIPO_CAMBIO"]
+
+    # Calculamos el valor promedio or operación.
+    df["valor_promedio"] = df["real"] / df["OPERACIONES"]
+
+    # Para cada año vamos a a neceitar 3 valores.
+    # Máximo, mínimo y promedio.
+    df = df.resample("YS")["valor_promedio"].agg(max="max", min="min", promedio="mean")
+
+    # Cambiamos de fecha a integral para el índice.
+    df.index = df.index.year
+
+    # El título cambia dependiendo el flujo.
+    if flujo == "Ingresos":
+        titulo = f"Evolución del monto promedio por remesa <b>hacia</b> México ({df.index.min()}-{df.index.max()})"
+        color = "#ffa726"
+    elif flujo == "Egresos":
+        titulo = f"Evolución del monto promedio por remesa <b>desde</b> México ({df.index.min()}-{df.index.max()})"
+        color = "#ba68c8"
+
+    # Haremos una gráfica tipo oruga.
+    # Cada año será representado por el
+    # promedio anual y su variabilidad.
+    fig = go.Figure()
+
+    x = list()
+    y = list()
+
+    for index, row in df.iterrows():
+        x.extend([index, index, None])
+        y.extend([row["min"], row["max"], None])
+
+    fig.add_traces(
+        go.Scatter(
+            x=x,
+            y=y,
+            mode="lines",
+            name="Rango anual (míniom-máximo)",
+            line_color="#FFFFFF",
+            line_width=4,
+        )
+    )
+
+    fig.add_traces(
+        go.Scatter(
+            x=df.index,
+            y=df["promedio"],
+            mode="markers",
+            name="Promedio anual",
+            marker_color=color,
+            marker_size=24,
+        )
+    )
+
+    fig.update_xaxes(
+        range=[df.index.min() - 0.7, df.index.max() + 0.7],
+        ticks="outside",
+        ticklen=10,
+        zeroline=False,
+        linecolor="#EEEEEE",
+        tickcolor="#EEEEEE",
+        linewidth=2,
+        showline=True,
+        showgrid=False,
+        mirror=True,
+        nticks=25,
+    )
+
+    fig.update_yaxes(
+        title=f"Pesos constantes (base, {FECHA_INFLACION})",
+        ticks="outside",
+        ticklen=10,
+        title_standoff=15,
+        linecolor="#EEEEEE",
+        tickcolor="#EEEEEE",
+        linewidth=2,
+        showgrid=True,
+        gridwidth=0.5,
+        showline=True,
+        mirror=True,
+        nticks=15,
+    )
+
+    fig.update_layout(
+        showlegend=True,
+        legend_borderwidth=1,
+        legend_bordercolor="#EEEEEE",
+        legend_x=0.99,
+        legend_y=0.98,
+        legend_xanchor="right",
+        legend_yanchor="top",
+        width=1920,
+        height=1080,
+        font_family="Inter",
+        font_color="#FFFFFF",
+        font_size=24,
+        title_text=titulo,
+        title_x=0.5,
+        title_y=0.965,
+        margin_t=80,
+        margin_l=140,
+        margin_r=40,
+        margin_b=125,
+        title_font_size=36,
+        plot_bgcolor=PLOT_COLOR,
+        paper_bgcolor=PAPER_COLOR,
+        annotations=[
+            dict(
+                x=0.02,
+                y=0,
+                xref="paper",
+                yref="paper",
+                xanchor="left",
+                yanchor="bottom",
+                bordercolor="#EEEEEE",
+                borderwidth=1.5,
+                borderpad=7,
+                bgcolor=PLOT_COLOR,
+                align="left",
+                text="<b>Nota:</b><br>El promedio, mínimo y máximo anuales calculados a partir<br>de los montos promedio mensuales en pesos constantes.",
+            ),
+            dict(
+                x=0.01,
+                y=-0.11,
+                xref="paper",
+                yref="paper",
+                xanchor="left",
+                yanchor="top",
+                text=f"Fuente: Banxico ({FECHA_FUENTE})",
+            ),
+            dict(
+                x=0.5,
+                y=-0.11,
+                xref="paper",
+                yref="paper",
+                xanchor="center",
+                yanchor="top",
+                text="Año de registro",
+            ),
+            dict(
+                x=1.01,
+                y=-0.11,
+                xref="paper",
+                yref="paper",
+                xanchor="right",
+                yanchor="top",
+                text="🧁 @lapanquecita",
+            ),
+        ],
+    )
+
+    fig.write_image(f"./remesas_valor_promedio_{flujo.lower()}.png")
+
+
 if __name__ == "__main__":
     plot_mensuales("Ingresos")
     plot_mensuales("Egresos")
@@ -774,3 +954,6 @@ if __name__ == "__main__":
 
     plot_real_anual("Ingresos")
     plot_real_anual("Egresos")
+
+    remesa_promedio("Ingresos")
+    remesa_promedio("Egresos")
